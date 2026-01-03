7a2a5b570e75eeb953679cbb4135ed9335484318
BRonen <brennopereira6@gmail.com>
2026-01-02 22:15:42 -0300

    test: uncomment useful test

--- raft/src/test/kotlin/archon/raft/NodeTest.kt
+++ raft/src/test/kotlin/archon/raft/NodeTest.kt
@@ -28,34 +28,34 @@ class LibraryTest {
         for (i in 0..100) simulator.run()
     }
 
-    // @Test
-    // fun electionImpasse() {
-    //     val simulator = Simulator<Timer, Message>(42)
+    @Test
+    fun electionImpasse() {
+        val simulator = Simulator<Timer, Message>(42)
 
-    //     val nodesCount = 3
-    //     val quorumSize = (nodesCount / 2) + 1
+        val nodesCount = 3
+        val quorumSize = (nodesCount / 2) + 1
 
-    //     for (i in 1..nodesCount) {
-    //         simulator.registerNode { nodeid, context ->
-    //             RaftNode<SimulationContext<Timer, Message>>(context, nodeid, quorumSize)
-    //         }
-    //     }
+        for (i in 1..nodesCount) {
+            simulator.registerNode { nodeid, context ->
+                RaftNode<SimulationContext<Timer, Message>>(context, nodeid, quorumSize)
+            }
+        }
 
-    //     for (nodeid in 0..<nodesCount) {
-    //         val node = simulator.nodes[nodeid]
-    //         require(node is RaftNode) { "invalid node found" }
+        for (nodeid in 0..<nodesCount) {
+            val node = simulator.nodes[nodeid]
+            require(node is RaftNode) { "invalid node found" }
 
-    //         node.term = 1
-    //         node.role = Role.Candidate
-    //         node.votesReceived = 1
-    //         node.votedFor = Option.Some(nodeid)
+            node.term = 1
+            node.role = Role.Candidate
+            node.votesReceived = 1
+            node.votedFor = Option.Some(nodeid)
 
-    //         val delay = node.context.random(50..100).toLong()
-    //         node.context.schedule(Timer.ElectionsTrigger(node.term, 0), delay)
-    //     }
+            val delay = node.context.random(50..100).toLong()
+            node.context.schedule(Timer.ElectionsTrigger(node.term, 0), delay)
+        }
 
-    //     simulator.dumpState()
+        simulator.dumpState()
 
-    //     for (i in 0..100) simulator.run()
-    // }
+        for (i in 0..100) simulator.run()
+    }
 }
